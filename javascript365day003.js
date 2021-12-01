c0e5d7638e63e7b5683b7c73bf5310b742a5b2d7
sseal3 <80580994+sseal3@users.noreply.github.com>
2021-11-30 19:26:30 -0500

    Day 003 Work in Progress

---
+++ javascript365day003.js
@@ -0,0 +1,5 @@
+function ani() {
+  document.getElementById('boxId').className = 'box box-ani';
+  document.getElementById('malletId').className = 'mallet mallet-ani';
+  document.getElementById('boardId').className = 'board board-ani';
+}
